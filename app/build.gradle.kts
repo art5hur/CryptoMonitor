c2058f241bc22cd4a0355e3880044b4e90239d05
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-25 20:42:37 -0300

    Início projeto Crypto Monitor - layout título, conteúdo e botão (componentizar)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,4 +63,13 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.activity:activity-ktx:1.7.0")
+
+    implementation("androidx.appcompat:appcompat:1.4.1")
+
+
+
 }
